5f3843b312049fd3271ae911e5eaaa93c5001dec
csnwc <Neill.Campbell@bristol.ac.uk>
2020-12-05 21:46:04 +0000

    Configurable version of quicks.c & Makefile

--- Code/quicks.c
+++ Code/quicks.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <math.h>
 
-int partition(int *a, int l, int r);
 void quicksort(int *a, int l, int r);
+int partition (int arr[], int low, int high);
+void sorted(int*a);
+int intcompare(const void *a, const void *b);
 
-#define NUM 100000
+#define NUM 250000
 
 int main(void)
 {
@@ -13,13 +16,36 @@ int main(void)
    int i;
    int a[NUM]; 
 
-   for(i=0; i<NUM; i++)
+#ifdef RAND
+   /* Random */
+   for(i=0; i<NUM; i++){
       a[i] = rand()%100;
-
+   }
+#endif
+#ifdef SORT
+   /* Pathologically sorted */
+   for(i=0; i<NUM; i++){
+      a[i] = i;
+   }
+#endif
+#ifndef BUILTIN
+   printf("quicksort()\n");
    quicksort(a, 0, NUM-1);
+#else
+   printf("qsort()\n");
+   qsort(a, NUM, sizeof(int), intcompare);
+#endif
+   sorted(a);
 
    return 0;
+}
 
+void sorted(int*a)
+{
+   int i;
+   for(i=0; i<NUM-1; i++){
+      assert(a[i] <= a[i+1]);
+   }
 }
 
 void quicksort(int *a, int l, int r)
@@ -38,7 +64,7 @@ int partition(int *a, int l, int r)
 
    int piv;
 
-   piv = a[l];
+   piv = (a[l]+a[r])/2;
    while(l<r){
       while(piv < a[r] && l<r) r--;
       if(r!=l){
@@ -55,4 +81,11 @@ int partition(int *a, int l, int r)
    a[r] = piv;
    return r;
    
+}
+
+int intcompare(const void *a, const void *b)
+{
+   const int *ia = (const int *)a;
+   const int *ib = (const int *)b;
+   return *ia - *ib;
 }
