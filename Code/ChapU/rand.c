2ce76b04ea23b3e4b5360215b9dfd6ea1b89d9e6
csnwc <Neill.Campbell@bristol.ac.uk>
2023-11-20 10:37:28 +0000

    Layout tweaks

--- Code/ChapU/rand.c
+++ Code/ChapU/rand.c
@@ -36,23 +36,17 @@ int rand_r(unsigned int* seed)
 {
   unsigned int next = *seed;
   int result;
-
   next *= 1103515245;
   next += 12345;
   result = (unsigned int) (next / 65536) % 2048;
-
   next *= 1103515245;
   next += 12345;
   result <<= 10;
   result ^= (unsigned int) (next / 65536) % 1024;
-
   next *= 1103515245;
   next += 12345;
   result <<= 10;
   result ^= (unsigned int) (next / 65536) % 1024;
-
   *seed = next;
-
   return result;
-
 }
